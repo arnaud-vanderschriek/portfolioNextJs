b40c52d5e973a25c96e0a38fbb7c5a7535f24cea
D <arnaud.vanderschrieck@gmail.com>
2023-06-13 18:07:07 +0200

    mobile version (not finished yet)

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -6,14 +6,14 @@ const Details = ({position, company, companyLink, time, address, work}) => {
   const ref = useRef(null)
 
   return (
-    <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
+    <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between md:w-[80%] ">
       <LiIcons reference={ref} />
       <motion.div 
         initial={{y:50}}
         whileInView={{y:0}}
         transition={{duration:0.5, type:"spring"}}
       >
-        <h3 className="capitalize font-bold text-2xl">{position}&nbsp;
+        <h3 className="capitalize font-bold text-2xl sm:text-xl xs-text-lg">{position}&nbsp;
           <a 
             href={companyLink}
             target="_blank"
@@ -22,10 +22,10 @@ const Details = ({position, company, companyLink, time, address, work}) => {
             @{company}
           </a>
         </h3>
-        <span className="capitalize font-medium text-dark/75 dark:text-light/75">
+        <span className="capitalize font-medium text-dark/75 dark:text-light/75 xs:text-sm">
           {time} | {address}
         </span>
-        <p className="font-medium w-full">
+        <p className="font-medium w-full md:text-sm">
           {work}
         </p>
       </motion.div>
@@ -42,15 +42,15 @@ const Experience = () => {
 
   return(
     <div className="my-64">
-      <h2 className="font-bold text-8xl mb-32 w-full text-center">
+      <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
         Experience
       </h2>
-      <div ref={ref} className="w-[75%] mx-auto relative">
+      <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full ">
         <motion.div 
           style={{scaleY: scrollYProgress }} 
-          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light"
+          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px] xs:left-[20px]"
         /> 
-        <ul className="w-full flex flex-col items-start justify-between ml-4">
+        <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
           <Details   
             position="Fullstack developer" 
             company="WebTree.tech"
@@ -77,7 +77,8 @@ const Experience = () => {
             companyLink="https://www.hommedemetier.be/"
             time="2021"
             address="Charleroi, Belgique"
-            work="Web application that facilitates contact between client and building professional."
+            work="Web application that facilitates contact between client and building professional.
+            As a fullstack developer, we have to maintain the platform, add new features and allow the user to have a good user interface experience."
           />      
           <Details   
             position="Fullstack developer" 
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -15,25 +15,25 @@ const FramerImage = motion(Image)
 const FeaturedProject = ({type, title, summary, img, link, github}) => {
   return (
     <article className="w-full mt-16 p-12 flex items-center justify-between rounded-3xl border 
-      border-solid border-dark bg-light shadow-2xl"
+      border-solid border-dark bg-light shadow-2xl lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4"
     >
-      <Link href={link} target="_blank" className="w-1/2 cursor-pointer overflow-hidden rounded-lg">
+      <Link href={link} target="_blank" className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full">
         <FramerImage src={img} alt={title} className="w-full h-auto"
           whileHover={{scale: 1.05}}
           transition={{duration: 0.2}}
         />
       </Link>
-      <div className="w-1/2 flex flex-col items-start justify-between pl-6">
-        <span className="text-primary font-medium text-xl">{type}</span>
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
+        <span className="text-primary font-medium text-xl dark:text-primaryDark xs:text-base">{type}</span>
         <Link href={link} target="_blank" className="hover:underline underline-offset-2">
-          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
+          <h2 className="my-2 w-full text-left text-4xl font-bold dark:text-light sm:text-sm">{title}</h2>
         </Link>
-        <p className="my-2 font-medium text-dark">
+        <p className="my-2 font-medium text-dark sm:text-sm">
           {summary}
         </p>
         <div className="mt-2 flex items-center">
           <Link href={github} target="_blank" className="w-10"><GithubIcon /></Link>
-          <Link href={link} target="_blank" className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold">
+          <Link href={link} target="_blank" className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark sm:px-4 sm:text-base">
             Visit Project
           </Link>
         </div>
@@ -45,7 +45,7 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
 const Projects = ({title, type, img, link, github}) => {
   return (
     <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid 
-      border-dark bg-light p-6 relative "
+      border-dark bg-light p-6 relative dark:bg-dark dark:border-light xs:p-4"
     >
       <Link href={link} target="_blank" className="w-full cursor-pointer overflow-hidden rounded-lg">
         <FramerImage src={img} alt={title} className="w-full h-auto"
@@ -78,8 +78,8 @@ const projects = () => {
       </Head>
       <main className="w-full mb-16 flex flex-col items-center justify-center">
         <Layout className="pt-16" >
-          <AnimatedText text="Imagination Trumps knowledge" />
-          <div className="grid grid-cols-12 gap-24">
+          <AnimatedText text="Imagination Trumps knowledge" className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl" />
+          <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
             <div className="col-span-12">
               <FeaturedProject
                 title="Crypto Screener Application"
