0d1724c4e409292397e9319fa3dda4ac5d216a1d
arnaud <arnaud.vanderschrieck@gmail.com>
2024-05-27 15:19:31 +0200

    adding same style on about section article

--- src/pages/about.js
+++ src/pages/about.js
@@ -54,7 +54,7 @@ const about = () => {
                 I believe that design is about more than just making things look pretty – it's about solving problems and 
                 creating intuitive, enjoyable experiences for users. 
               </p>
-              <p>
+              <p className="my-4 font-medium">
                 Whether I'm working on a website or 
                 other digital product, I bring my commitment to design excellence and user-centered thinking to 
                 every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
